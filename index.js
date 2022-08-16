984433378cbb6670754cb4281e50ba149364d11f
lazikaz <souflakey@gmail.com>
2022-08-16 16:02:58 -0600

    enabled cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require("express")
 const app = express()
 const path = require("path")
+const cors = require("cors")
 
 app.use(express.static(path.join(__dirname, "build")))
+app.use(cors())
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "build", "index.html"))
 })
 
-app.listen(3001)
+app.listen(process.env.PORT)
