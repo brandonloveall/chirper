8070f8f454a9224cf4cc7af578f3ce32d22b5935
lazikaz <souflakey@gmail.com>
2022-08-19 11:41:32 -0600

    sign up box starting to work

--- index.js
+++ index.js
@@ -1,13 +1,28 @@
+require("dotenv").config()
 const express = require("express")
 const app = express()
 const path = require("path")
 const cors = require("cors")
+const {Sequelize} = require("sequelize")
+const sequelize = new Sequelize(process.env.POSTGRES)
 
 app.use(express.static(path.join(__dirname, "build")))
 app.use(cors())
 
+//PAGE
+
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "build", "index.html"))
 })
+
+// 
+
+//SIGN UP API
+
+app.get("/api/usernamecheck/:username", (req, res) => {
+    console.log("caught")
+    res.status(200).send("caught it")
+})
+
 
 app.listen(process.env.PORT || 3001)
