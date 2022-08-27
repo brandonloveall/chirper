7a7bf9068dc9171b6d0a93546ead5eea51344979
lazikaz <souflakey@gmail.com>
2022-08-27 13:16:10 -0600

    changed cookies to username and id and fixed the pop up  background not covering everything

--- index.js
+++ index.js
@@ -25,18 +25,23 @@ app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "build", "index.html"))
 })
 
+app.get("/c/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "build", "index.html"))
+})
+
 // 
 
 //SIGN UP API
 
 app.get("/api/usernamecheck/:username", (req, res) => {
+    console.log("hi")
     sequelize.query(`SELECT username FROM users WHERE username = '${req.params.username}'`).then((dbRes) => {res.status(200).send(dbRes[0])})
 })
 
 app.post("/api/signup", (req, res) => {
     bcrypt.hash(req.body.password, 10, (err, hash) => {
-        sequelize.query(`INSERT INTO users (username, password_hash, picture, backgroundimg) VALUES ('${req.body.username}', '${hash}', 'https://freesvg.org/img/Colorful-Bird-Silhouette.png', 'https://vcdn.bergfex.at/images/resized/76/819c726eeadb1576_dd17e5de1d43fa3c@2x.jpg')`)
-    .then(dbRes => {res.status(201).send({hash: hash, icon: "https://freesvg.org/img/Colorful-Bird-Silhouette.png"})})
+        sequelize.query(`INSERT INTO users (username, password_hash, picture, backgroundimg) VALUES ('${req.body.username}', '${hash}', 'https://freesvg.org/img/Colorful-Bird-Silhouette.png', 'https://vcdn.bergfex.at/images/resized/76/819c726eeadb1576_dd17e5de1d43fa3c@2x.jpg'); SELECT * FROM users WHERE username = '${req.body.username}'`)
+    .then(dbRes => {res.status(201).send({username: dbRes[0][0].username, id: dbRes[0][0].id})})
     })
 })
 
@@ -50,7 +55,7 @@ app.get("/api/login", (req, res) => {
         if(dbRes[0].length !== 0){
             bcrypt.compare(password, dbRes[0][0].password_hash).then((result) => {
                 if(result){
-                    res.status(200).send({username: dbRes[0][0].id})
+                    res.status(200).send({username: dbRes[0][0].username, id: dbRes[0][0].id})
                 }else{
                     res.status(200).send("incorrect password")
                 }
